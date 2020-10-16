97219f8c10fb7e078b8611eea88e3eb3232d9f82
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-10-16 12:07:34 -0400

    ANCESTOR SOLUTION

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,21 @@
-
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    ancestor_graph = {}
+    
+    for parent, child in ancestors:
+        if child in ancestor_graph:
+            ancestor_graph[child].add(parent)
+        else:
+            ancestor_graph[child] = set()
+            ancestor_graph[child].add(parent)
+
+    if starting_node not in ancestor_graph.keys():
+        return -1
+    
+    current = starting_node
+    parent = min(ancestor_graph[current])
+
+    while parent in ancestor_graph.keys():
+        current = parent
+        parent = min(ancestor_graph[current])
+    
+    return parent
